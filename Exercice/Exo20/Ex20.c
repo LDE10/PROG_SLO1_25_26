4ed6b97ffd5e904f2d55d5289a9ca05edb9f50ea
Luc Derré <lucderre@etmlnet.local>
2026-03-27 11:59:22 +0100

    FINI

--- Exercice/Exo20/Ex20.c
+++ Exercice/Exo20/Ex20.c
@@ -9,33 +9,34 @@
 int main(void)
 {
 	char rep;
-	int chaine[100];
-	int chiffre1 = 0;
-	float chiffre2 = 0;
 	FILE *fp1;						// Descripteur de fichier 
 	char *FileName = "Ex20.txt";	// Nom du fichier
 
+	char ligne[100];
+
+	char *texte = "Exercice 20 Luc Derre";
+	int chiffre = 12345;
+	float Nbr = 521.13;
+
 	printf("Fichier %s \n", FileName);
-	printf ("Ex20 Derre Luc \n");
 	
 	// ouverture pour écriture séquentielle
 	fp1 = fopen("Ex20.txt", "w");
 
-	fprintf(fp1,"Exercice 20 Luc Derre\n%d\n%f", 12345, 521.13);
+	fprintf(fp1,"%s\n%d\n%f\n", texte, chiffre, Nbr);	//écriture dans le fichier
 	
 	fclose(fp1);
+
 	// 2ème phase relecture
 	fp1 = fopen("Ex20.txt", "r");
-	fgets(chaine,100,fp1);
-	printf("%s", chaine);
-	fclose(fp1);
 
-	fp1 = fopen("Ex20.txt", "r");
-	fscanf(fp1, "%d\n \n%f", chiffre1, chiffre2);
-	printf("%d\n%f\n", chiffre1, chiffre2);
+	while (fgets(ligne, 100, fp1) != NULL)		//Tant que la lecture ne rencontre RIEN continue de lire
+	{
+		printf("%s", ligne);
+	}
 
 	fclose(fp1);
-	
+
 	// Sorte de pause
 	printf ("Enter pour terminer \n");
 	scanf_s("%c", &rep, 1);
