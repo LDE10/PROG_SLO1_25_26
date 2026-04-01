87cfd9464f6ae35d625f48d53389635f0268c80d
Luc Derré <lucderre@etmlnet.local>
2026-04-01 16:06:21 +0200

    A finir

--- Exercice/Exo21/Ex21.c
+++ Exercice/Exo21/Ex21.c
@@ -20,7 +20,10 @@ int main(void)
 
 	int test;
 	char cmpt = 0;
-	int tableau1[10];
+	int tableau1[100];
+	int tableau2[10];
+
+	long offset;
 
 	printf ("Ex21 Luc Derre\n");
 	
@@ -36,27 +39,31 @@ int main(void)
 	fp1 = fopen("Ex21_1.txt", "rb");
 
 	// Boucle d'affichage en hexa et ASCII du contenu avec fgetc
-
-
-	for(char i = 0; i <= 5; i++)
+	for (char i = 0; i < 2; i++)
 	{
-		for (char i = 0; i < 10; i++)
+		for (char i = 0; i < 11; i++)
 		{
 			tableau1[i] = getc(fp1);
 			printf("%02x ", tableau1[i]);
 		}
-		for (char i = 0; i <= 10; i++)
+		for (char i = 0; i <= 11; i++)
 		{
-			tableau1[i] = getc(fp1);
 			printf("%c ", tableau1[i]);
 			if (i == 10)
 			{
 				printf("\n");
 			}
+			cmpt++;
+		}
+		if (i == 3)
+		{
+
+		}
+		if (tableau1[i] < 0x20)
+		{
+			printf("*");
 		}
 	}
-
-	
 
 	fclose(fp1);
 
